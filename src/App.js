d5535f798f3ada4bfaade60ba194f1f5c7278519
alamin <webalamin4@gmail.com>
2022-07-27 10:21:36 +0600

    ready components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Service from './components/Service';
 
 function App() {
   return (
     <div >
-      <h1 className='text-3xl font-bold underline'>hlw</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
